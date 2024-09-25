139813b1146cf893a683e314a8fbe1bee4aca773
sopyanbadawi <sopyanbadawi.com>
2024-09-25 08:16:08 +0700

    Pemilihan Bilangan 2

--- PemilihanBilangan04.java
+++ PemilihanBilangan04.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
     System.out.println("Masukkan sebuah angka: ");
     int angka = sc.nextInt();
     
-    if (angka % 2 ==0) {
-        System.out.println("Angka "  + angka + " termasuk bilangan genap");
-    }
-    else {
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-    }
+    String hasil = (angka % 2 == 0 ) ? "bilangan genap" 
+                                    : "bilangan ganjil";
+    System.out.println("Angka " + angka + " adalah " + hasil);
+
     }
  }
